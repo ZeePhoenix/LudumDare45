36adb73d85e389fc90e827c3dd29447aaf11047e
Z33Phoenix <zcupec@gmail.com>
2019-10-05 21:44:18 -0400

    Updated Player Movement
    Added more refined player movement, and animated the player

--- PHDRock/Scripts/KinematicBody2D.gd
+++ PHDRock/Scripts/KinematicBody2D.gd
@@ -1,7 +1,11 @@
 extends KinematicBody2D
 
 export (int) var speed = 200;
-var velocity = Vector2()
+var velocity = Vector2();
+
+var facing = "down";
+onready var sprite = $sprite;
+var moving = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,16 +13,41 @@ func _ready():
 
 func get_input():
 	velocity = Vector2()
-	if Input.is_action_pressed('ui_left'):
-		velocity.x -= 1
-	if Input.is_action_pressed('ui_right'):
-		velocity.x += 1
-	if Input.is_action_pressed('ui_up'):
-		velocity.y -= 1
-	if Input.is_action_pressed('ui_down'):
-		velocity.y += 1
-	velocity = velocity.normalized() * speed
+	if Input.is_action_pressed('ui_left') && velocity.y == 0:
+		velocity.x -= 1;
+		facing = "left";
+	if Input.is_action_pressed('ui_right') && velocity.y == 0:
+		velocity.x += 1;
+		facing = "right";
+	if Input.is_action_pressed('ui_up') && velocity.x == 0:
+		velocity.y -= 1;
+		facing = "up";
+	if Input.is_action_pressed('ui_down') && velocity.x == 0:
+		velocity.y += 1;
+		facing = "down";
+	velocity = velocity.normalized() * speed;
 	
 func _physics_process(delta):
 	get_input()
-	velocity = move_and_slide(velocity)
+	if velocity.x == 0 && velocity.y == 0:
+		moving = false;
+	else:
+		moving = true;
+	updateFrames()
+	velocity = move_and_slide(velocity);
+	
+func updateFrames():
+	if facing == "down":
+		sprite.play("down");
+	elif facing == "up":
+		sprite.play("up");
+	elif facing == "right":
+		sprite.play("right");
+	elif facing == "left":
+		sprite.play("left");
+	
+	if moving:
+		sprite.playing = true;
+	else:
+		sprite.playing = false;
+		sprite.frame = 0;
